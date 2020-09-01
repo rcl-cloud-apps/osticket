95e3e44d61e6b75a64f49d612328f15de606e091
Anil Ripla <rcl@rclapp.com>
2020-09-01 04:36:45 -0400

    initial commit

---
+++ setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="https://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.5.1.min.js?f4f5bc6"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?f4f5bc6"></script>
+    <script type="text/javascript" src="js/setup.js?f4f5bc6"></script>
+    <script type="text/javascript" src="js/tips.js?f4f5bc6"></script>
+</body>
+</html>
